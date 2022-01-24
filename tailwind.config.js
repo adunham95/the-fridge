095fd2ea7cbe90530164caaa09907a8efef23067
adunham95 <adunham95@gmail.com>
2022-01-24 16:04:51 -0500

    Added theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,16 @@
+const { default: theme } = require('./styles/theme');
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        'brand': {
-          '50': '#f2fbfd', 
-          '100': '#e6f7fb', 
-          '200': '#bfeaf5', 
-          '300': '#99ddee', 
-          '400': '#4dc4e2', 
-          '500': '#00abd5', 
-          '600': '#009ac0', 
-          '700': '#0080a0', 
-          '800': '#006780', 
-          '900': '#005468',
-      },
+        ...theme.COLOR,
       },
     },
   },
   plugins: [],
-}
+};
