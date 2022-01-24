2f2f1d67160508e95e5ab2a457e80f45d1b50499
adunham95 <adunham95@gmail.com>
2022-01-24 11:03:23 -0500

    Hide SW on dev

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-})
+console.log(process.env.NODE_ENV)
+
+if(process.env.NODE_ENV !== 'development'){
+  module.exports = withPWA({
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+    },
+  })
+}
+else{
+  module.exports = nextConfig
+}
