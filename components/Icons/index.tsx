533c045f6ca51528f91850f109e4acd2a6121e9d
adunham95 <adunham95@gmail.com>
2022-01-24 07:45:10 -0500

    Added icons

---
+++ components/Icons/index.tsx
@@ -0,0 +1,29 @@
+import IconHouse from "./Icon-house";
+import IconUser from "./Icon-User";
+
+export interface IIconProps{
+    name: String
+}
+
+export enum EIcons{
+    HOME = 'Home',
+    USER = 'User'
+} 
+
+export const IconList = Object.values(EIcons);
+
+const Icon = (props: IIconProps) => {
+    const {
+        name
+    } = props
+    switch (name) {
+        case EIcons.HOME:
+            return <IconHouse {...props}/>;
+        case EIcons.USER:
+            return <IconUser {...props}/>;
+        default:
+            return <></>
+    }
+}
+
+export default Icon
