a1f18ea5b21602e495f873964ddb3bfc68042e5b
adunham95 <adunham95@gmail.com>
2022-01-25 07:35:24 -0500

    Added new comment

--- components/Post/Comments.tsx
+++ components/Post/Comments.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import { IComment } from '../../models/CommentModel';
+import { IUser } from '../../models/UserModel';
+import { Avatar } from '../Avatar/Avatar';
 
 interface IProps {
   comments: Array<IComment>;
@@ -6,18 +9,46 @@ interface IProps {
   allowComment?: boolean;
 }
 
+const myUser: IUser = {
+  id: 'adrian',
+  name: 'Adrian Dunham',
+};
+
 const Comments = ({ comments = [], limit, allowComment = false }: IProps) => {
   const filteredComments = limit ? comments.slice(0, limit) : comments;
   return (
-    <>
+    <div>
       {filteredComments.map((c) => (
         <div key={c.id} className="text-xs pb-1">
           <span className=" font-bold">{c.commentAuthor.name}: </span>
           <span>{c.message}</span>
         </div>
       ))}
-    </>
+      {allowComment && <NewComment />}
+    </div>
   );
 };
+
+interface INewComment {
+  newComment?: () => void;
+}
+
+function NewComment({ newComment }: INewComment) {
+  const [comment, setComment] = useState('');
+  return (
+    <div className="flex">
+      <Avatar name={myUser.name} />
+      <input
+        onChange={(e) => setComment(e.target.value)}
+        className="w-full border-b-2 border-black"
+      />
+      <button
+        className={`${comment === '' ? 'text-gray-400' : 'text-green-500'}`}
+      >
+        Post
+      </button>
+    </div>
+  );
+}
 
 export default Comments;
