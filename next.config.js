3f94ad7ea8e4f8c13ba09cde45d1b006c43438f3
adunham95 <adunham95@gmail.com>
2022-01-25 14:05:09 -0500

    Added api route

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
 };
 
 // console.log(process.env.NODE_ENV)
