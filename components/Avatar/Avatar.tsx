469fe23d5b4770f372029c5b3c8bd6eb18c1c20d
adunham95 <adunham95@gmail.com>
2022-01-25 07:35:11 -0500

    Added no flex

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -10,7 +10,7 @@ export const Avatar = ({ name, color = theme.COLORS.blue[500] }: IProps) => {
   return (
     <span
       style={{ backgroundColor: color }}
-      className=" h-8 w-8 rounded-full inline-flex justify-center items-center mr-1 text-white"
+      className=" h-8 w-8 rounded-full inline-flex justify-center items-center mr-1 text-white flex-none"
     >
       {name[0]}
     </span>
