76a6a7341f0b527f475e41ef2f968e4637e753f0
adunham95 <adunham95@gmail.com>
2022-01-25 21:08:59 -0500

    Reverted to useing global state

--- stores/store.ts
+++ stores/store.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { configureStore } from '@reduxjs/toolkit';
 import orgReducer from '../reducers/orgReducer';
 import postReducer from '../reducers/postReducer';
 import userReducer from '../reducers/userReducer';
 import { IGlobalState } from './globalState';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const store = configureStore({
   reducer: {
     user: userReducer,
