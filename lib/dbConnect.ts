13c93f7814da8ae2b8df4ffdc4befb97305e6925
adunham95 <adunham95@gmail.com>
2022-01-26 09:15:47 -0500

    Added generated db string

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,10 +1,24 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
+let MONGODB_URI = '';
+if (
+  process.env.MDB_USERNAME &&
+  process.env.MONGODB_URL &&
+  process.env.MDB_PASSWORD &&
+  process.env.MDB_DBNAME
+) {
+  MONGODB_URI = process.env.MONGODB_URL.replace(
+    '{MDB_USERNAME}',
+    process.env.MDB_USERNAME,
+  )
+    .replace('{MDB_PASSWORD}', process.env.MDB_PASSWORD)
+    .replace('{MDB_DBNAME}', process.env.MDB_DBNAME);
+}
 
 console.log(MONGODB_URI);
 
-if (!MONGODB_URI) {
+if (!MONGODB_URI || MONGODB_URI === '') {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local',
   );
