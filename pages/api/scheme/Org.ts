fa14cbc3915131213984693eab54d51c9c546d6e
adunham95 <adunham95@gmail.com>
2022-01-31 11:16:11 -0500

    Added option to update org

--- pages/api/scheme/Org.ts
+++ pages/api/scheme/Org.ts
@@ -1,16 +1,26 @@
 import { gql } from 'apollo-server-micro';
 import dbConnect from '../../../utils/dbConnect';
 import { OrgModel } from '../../../models/OrgModel_Server';
+import { Types } from 'mongoose';
 
 export const typeDef = gql`
   type Org {
     id: String!
     name: String!
     groups: [Group!]
+    defaultPostGroups: [String!]
+    defaultPostSettings: [String!]
   }
 
   input OrgInput {
     name: String!
+    defaultPostGroups: [String!]
+    defaultPostSettings: [String!]
+  }
+
+  input UpdateOrgInput {
+    id: String!
+    name: String
     defaultPostGroups: [String!]
     defaultPostSettings: [String!]
   }
@@ -21,6 +31,7 @@ export const typeDef = gql`
 
   extend type Mutation {
     createOrg(input: OrgInput!): Org!
+    updateOrg(input: UpdateOrgInput): Org!
   }
 `;
 
@@ -47,6 +58,32 @@ export const resolvers = {
         });
         const newOrgFromDB = await newOrg.save();
         return newOrgFromDB;
+      } catch (error) {
+        throw error;
+      }
+    },
+    updateOrg: async (_: any, args: any) => {
+      try {
+        await dbConnect();
+        console.log(args);
+        const update: any = {};
+        if (args.input?.name) {
+          update.name = args.input.name;
+        }
+        if (args.input?.defaultPostGroups) {
+          update.defaultPostGroups = args.input.defaultPostGroups;
+        }
+        if (args.input?.defaultPostSettings) {
+          update.defaultPostSettings = args.input.defaultPostSettings;
+        }
+        console.log(update);
+        const updatedOrg = OrgModel.findByIdAndUpdate(
+          Types.ObjectId(args.input.id),
+          update,
+          { upsert: true, returnDocument: 'after' },
+        );
+        console.log(updatedOrg);
+        return updatedOrg;
       } catch (error) {
         throw error;
       }
