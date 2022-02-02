f29aed78a7d0844461aca8f32caa76441ccd9563
adunham95 <adunham95@gmail.com>
2022-02-02 14:42:28 -0500

    Implimentaed basic Form to create groups

--- pages/admin/newGroup.tsx
+++ pages/admin/newGroup.tsx
@@ -1,7 +1,41 @@
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import { UserPermissionDetails } from '../../models/UserPermission';
 
 const AdminGroup = () => {
+  const [selectedPermission, setSelectedPermission] = useState<Array<string>>(
+    [],
+  );
+  // eslint-disable-next-line prettier/prettier
+  const [orgs, setOrgs] = useState<Array<{ orgID: string, name: string }>>([]);
+  const { data: session } = useSession();
+  const myUser = session?.user;
+
+  useEffect(() => {
+    console.log(myUser);
+    const orgList =
+      myUser?.orgs.map((o) => {
+        return {
+          orgID: o.org.id,
+          name: o.org.name,
+        };
+      }) || [];
+
+    console.log(orgList);
+    setOrgs(orgList);
+  }, [myUser]);
+
+  function updatePermission(permString: string) {
+    let newPermissions = [...selectedPermission];
+    if (newPermissions.includes(permString)) {
+      newPermissions = newPermissions.filter((g) => g !== permString);
+    } else if (!newPermissions.includes(permString)) {
+      newPermissions = [...newPermissions, permString];
+    }
+    setSelectedPermission(newPermissions);
+  }
+
   return (
     <Layout>
       <>
@@ -14,18 +48,79 @@ const AdminGroup = () => {
         </header>
         <main className="pt-2 px-3">
           <form>
-            <div>
-              <label htmlFor="name">Group Name</label>
-              <input id="name" placeholder="Admin" />
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="org"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Group Belongs To:
+                <p></p>
+              </label>
+              <select
+                id="org"
+                name="org"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-brand-500 focus:border-brand-500 sm:text-sm"
+              >
+                <option>Select Org</option>
+                {orgs.map((o) => (
+                  <option value={o.orgID} key={o.orgID}>
+                    {o.name}
+                  </option>
+                ))}
+              </select>
             </div>
-            <div>
-              <h2>Group Permissions</h2>
-              {Object.keys(UserPermissionDetails).map((key) => (
-                <div key={key}>
-                  <span>{UserPermissionDetails[key].title}</span>
-                  <p>{UserPermissionDetails[key].description}</p>
-                </div>
-              ))}
+            <div className="pt-2">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Group Name
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Admin"
+                  className="focus:ring-brand-500 focus:brand-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="pt-2">
+              <h2 className="text-sm font-medium text-gray-700">
+                Group Permissions
+              </h2>
+              <div className="flex flex-wrap">
+                {Object.keys(UserPermissionDetails).map((key) => {
+                  const isChecked = selectedPermission.includes(key);
+                  return (
+                    <span key={key} className="w-1/2 sm:w-1/3 md:w-1/4 p-1">
+                      <input
+                        className="sr-only"
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={() => updatePermission(key)}
+                        id={key}
+                      />
+                      <label
+                        htmlFor={key}
+                        className={`p-2 block border  h-full rounded transition cursor-pointer ${
+                          isChecked
+                            ? ' border-brand-300 shadow-md shadow-brand-200'
+                            : 'border-gray-200'
+                        }`}
+                      >
+                        <span className=" text-base border-b block w-full border-grey-200">
+                          {UserPermissionDetails[key].title}
+                        </span>
+                        <p className="text-sm">
+                          {UserPermissionDetails[key].description}
+                        </p>
+                      </label>
+                    </span>
+                  );
+                })}
+              </div>
             </div>
           </form>
         </main>
@@ -33,5 +128,7 @@ const AdminGroup = () => {
     </Layout>
   );
 };
+
+AdminGroup.auth = true;
 
 export default AdminGroup;
