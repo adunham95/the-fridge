02e5865a438ae8ba1110cb598ddea46ed79e3718
adunham95 <adunham95@gmail.com>
2022-02-14 15:48:51 -0500

    Added dynamic height

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -4,16 +4,22 @@ import theme from '../../theme/theme.json';
 type IProps = {
   name?: string,
   color?: string,
+  height?: string,
+  width?: string,
+  className?: string,
 };
 
 export const Avatar = ({
   name = 'A',
   color = theme.COLORS.blue[500],
+  height = 'h-8',
+  width = 'h-8',
+  className = '',
 }: IProps) => {
   return (
     <span
       style={{ backgroundColor: color }}
-      className=" h-8 w-8 rounded-full inline-flex justify-center items-center mr-1 text-white flex-none"
+      className={`${height} ${width} rounded-full inline-flex justify-center items-center mr-1 text-white flex-none ${className}`}
     >
       {name[0]}
     </span>
