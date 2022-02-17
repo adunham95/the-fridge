f8425358ca64842fd686c920db277d40c3b79df5
adunham95 <adunham95@gmail.com>
2022-02-17 06:59:00 -0500

    Updated icons

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -1,16 +1,23 @@
+import IconArrow from './Icon-arrow';
+import IconArrowCircle from './Icon-Arrow-Circle';
 import IconBell from './Icon-Bell';
 import IconBellAlert from './Icon-Bell-Alert';
 import IconBook from './Icon-book';
+import IconBullhorn from './Icon-Bullhorn';
 import IconCalender from './Icon-Calender';
 import IconCheckCircle from './Icon-Check-Circle';
+import IconComment from './Icon-Comment';
 import IconCowbell from './Icon-Cowbwll';
 import IconExclamation from './Icon-Exclamation';
 import IconExclamationTriangle from './Icon-ExlamationTriangle';
+import IconExternal from './Icon-External';
 import IconGear from './Icon-gear';
+import IconHeart from './Icon-Heart';
 import IconHouse from './Icon-house';
 import IconInfo from './Icon-Info';
 import IconLock from './Icon-lock';
 import IconPalette from './Icon-Palette';
+import IconPlane from './Icon-Plane';
 import IconThumbUp from './Icon-Thumb-Up';
 import IconUser from './Icon-User';
 import IconWarning from './Icon-Warning';
@@ -39,6 +46,13 @@ export enum EIcons {
   CHECK_CIRCLE = 'CheckCircle',
   THUMB_UP = 'ThumbUp',
   PALETTE = 'Palette',
+  BULLHORN = 'Bullhorn',
+  ARROW_CIRCLE = ' ArrowCircle',
+  ARROW = 'Arrow',
+  COMMENT = 'Comment',
+  EXTERNAL = 'External',
+  HEART = 'Heart',
+  PLANE = 'Plane',
 }
 
 export const IconList = Object.values(EIcons);
@@ -78,6 +92,20 @@ const Icon = (props: IIconProps) => {
       return <IconThumbUp {...props} />;
     case EIcons.PALETTE:
       return <IconPalette {...props} />;
+    case EIcons.BULLHORN:
+      return <IconBullhorn {...props} />;
+    case EIcons.ARROW_CIRCLE:
+      return <IconArrowCircle {...props} />;
+    case EIcons.ARROW:
+      return <IconArrow {...props} />;
+    case EIcons.COMMENT:
+      return <IconComment {...props} />;
+    case EIcons.EXTERNAL:
+      return <IconExternal {...props} />;
+    case EIcons.HEART:
+      return <IconHeart {...props} />;
+    case EIcons.PLANE:
+      return <IconPlane {...props} />;
     default:
       return <></>;
   }
