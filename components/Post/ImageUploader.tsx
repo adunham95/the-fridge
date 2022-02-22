af244263fa29c0ed25f3f73c15a2d29bcda91203
adunham95 <adunham95@gmail.com>
2022-02-22 08:58:16 -0500

    Changed to use custom endpoint

--- components/Post/ImageUploader.tsx
+++ components/Post/ImageUploader.tsx
@@ -5,6 +5,7 @@ import IconImage from '../Icons/Icon-Image';
 import { useToast } from '../Toast/ToastContext';
 import theme from '../../theme/theme.json';
 import { EIcons } from '../Icons';
+import { randomFileName, resizeImage } from '../../util/fileHelpers';
 
 interface IImageUploadProps {
   id: string;
@@ -52,44 +53,35 @@ export function ImageUploader({
   async function onLoad(event: React.ChangeEvent<HTMLInputElement>) {
     console.log(event.target);
     console.log(event.target.files);
-    const maxFileSize = 10485760;
 
     const files = event?.target?.files || [];
     const src: Array<IUploadedImage> = [];
 
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
-      console.log(file);
-      if (file.size > maxFileSize) {
-        addToast(
-          `${file.name} is too large`,
-          theme.BASE_COLOR.error,
-          EIcons.EXCLAMATION_TRIANGLE,
-        );
-        return;
-      }
       addToast(
         `Uploading ${file.name}`,
         theme.BASE_COLOR['brand-blue'],
         EIcons.INFO,
       );
-      // src.push({ src: URL.createObjectURL(file) });
+      console.log('preResizeFile', file);
+      const config = {
+        file: file,
+        maxSize: 1000,
+        newFileName: `${randomFileName(10)}.jpg`,
+      };
+      const resizedImage = await resizeImage(config);
+      console.log('postResizeFile', resizedImage);
       const data = new FormData();
-      data.append('file', file);
-      data.append('upload_preset', 'fridge-images');
-
       // @ts-ignore
-      data.append('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_NAME);
+      data.append('images', resizedImage);
 
-      // @ts-ignore
-      const response = await fetch(
-        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_NAME}/upload`,
-        {
-          method: 'post',
-          body: data,
-        },
-      );
+      const response = await fetch(`/api/upload-images`, {
+        method: 'post',
+        body: data,
+      });
       const img = await response.json();
+      console.log(img);
       if (img.error) {
         addToast(
           'Error Uploading image',
@@ -127,7 +119,7 @@ export function ImageUploader({
       />
       <label
         htmlFor={id}
-        className="flex rounded-md bg-slate-500 text-white px-2 py-1 py-1 mr-1 mb-1"
+        className="flex rounded-md bg-slate-500 text-white px-2 py-1 mr-1 mb-1"
       >
         <IconImage height={20} width={20} />
         <span className="pl-2 text-sm">Images</span>
