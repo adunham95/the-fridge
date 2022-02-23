152439fc729bc241d07c856b1d78f869928d3944
adunham95 <adunham95@gmail.com>
2022-02-23 08:41:19 -0500

    Added rediect

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin/org/editusergroup',
+        destination: '/admin/org',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 // console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV !== 'development') {
   module.exports = withPWA({
+    nextConfig,
     pwa: {
       dest: 'public',
       runtimeCaching,
