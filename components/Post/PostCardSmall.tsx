0c3745401a0f3478a420669aca55a7cae9c1a3e0
adunham95 <adunham95@gmail.com>
2022-03-10 07:16:40 -0500

    Added text only posts

--- components/Post/PostCardSmall.tsx
+++ components/Post/PostCardSmall.tsx
@@ -1,6 +1,6 @@
 // @flow
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IPost } from '../../models/PostModel';
 import { Avatar } from '../Avatar/Avatar';
 import IconImages from '../Icons/Icon-Images';
@@ -11,19 +11,36 @@ export function PostCardSmall({
   image = [],
   postedBy,
 }: IPost) {
+  const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    if (image.length > 0) {
+      setStyle({ backgroundImage: `url(${image[0]})` });
+    }
+  }, [image]);
+
   return (
     <Link href={`/post/${id}`} passHref>
       <a id={id} className="aspect-square w-1/2  md:w-1/3 lg:w-1/4 p-1">
         <div
-          style={{ backgroundImage: `url(${image[0]})` }}
-          className="bg-green-200 rounded-md bg-no-repeat bg-cover w-full h-full relative"
+          style={style}
+          className="bg-gradient-to-br from-brand-300 to-brand-blue-500 rounded-md bg-no-repeat bg-cover w-full h-full relative"
         >
           <div className="absolute top-1 left-1">
             <Avatar name={postedBy.name} color={postedBy.accountColor} />
           </div>
-          {description !== '' && (
+          {description !== '' && image.length > 1 && (
             <div className="p-1 absolute bottom-0 bg-white bg-opacity-80 w-full border-t border-white text-sm">
-              <p className=" text-slate-700">{description}</p>
+              <p className=" text-slate-700 truncate text-ellipsis">
+                {description}
+              </p>
+            </div>
+          )}
+          {description !== '' && image.length === 0 && (
+            <div className="absolute inset-[2.5rem] text-sm flex justify-center items-start overflow-hidden">
+              <p className="p-1 text-slate-700 bg-white bg-opacity-20 rounded">
+                {description}
+              </p>
             </div>
           )}
           {image.length > 1 && (
