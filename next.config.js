e948d23455ccac665020ad9da72b646d8f25eab8
Adrian Dunham <adunham95@gmail.com>
2022-05-28 17:45:44 -0400

    Added sentry (#17)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const { withSentryConfig } = require('@sentry/nextjs');
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
@@ -13,16 +14,23 @@ const nextConfig = {
   },
 };
 
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
 // console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV !== 'development') {
-  module.exports = withPWA({
-    nextConfig,
-    pwa: {
-      dest: 'public',
-      runtimeCaching,
-    },
-  });
+  module.exports = withSentryConfig(
+    withPWA({
+      nextConfig,
+      pwa: {
+        dest: 'public',
+        runtimeCaching,
+      },
+    }),
+    sentryWebpackPluginOptions,
+  );
 } else {
   module.exports = nextConfig;
 }
